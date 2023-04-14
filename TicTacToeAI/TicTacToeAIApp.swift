9dd98f2016764d9d6ca89ff3535e29aa8d52e42d
Cenk Bahadir Cark <79763515+cenkbcark@users.noreply.github.com>
2023-04-15 00:14:25 +0300

    Initial Commit

---
+++ TicTacToeAI/TicTacToeAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeAIApp.swift
+//  TicTacToeAI
+//
+//  Created by Cenk Bahadır Çark on 15.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
